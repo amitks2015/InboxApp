04d5b5000733db034d8c58507e72fd2a876d8573
Amit Singh <amit.mot.08@gmail.com>
2022-08-03 06:48:58 +1000

    Divider animation and accessibility

--- app/src/main/java/com/example/inboxapp/inbox/EmailList.kt
+++ app/src/main/java/com/example/inboxapp/inbox/EmailList.kt
@@ -1,6 +1,7 @@
 package com.example.inboxapp.inbox
 
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -8,7 +9,13 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.CustomAccessibilityAction
+import androidx.compose.ui.semantics.customActions
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
+import com.example.inboxapp.R
 import com.example.inboxapp.model.Email
 import com.example.inboxapp.model.InboxEvent
 
@@ -39,7 +46,26 @@ fun EmailList(
                 }
             )
 
+            val dividerAlpha by animateFloatAsState(
+                targetValue = if(dismissState.currentValue == DismissValue.Default) {
+                    1f
+                } else {
+                    0f
+                },
+                animationSpec = tween(300)
+            )
+
+            val deleteLabel = stringResource(id = R.string.cd_delete_email)
+
             SwipeToDismiss(
+                modifier = Modifier.semantics {
+                    customActions = listOf(
+                        CustomAccessibilityAction(deleteLabel) {
+                            eventListener(InboxEvent.DeleteEvent(email.id))
+                            true
+                        }
+                    )
+                },
                 state = dismissState,
                 directions = setOf(DismissDirection.StartToEnd),
                 dismissThresholds = {
@@ -47,19 +73,26 @@ fun EmailList(
                 },
                 background = {
                     EmailItemBackground(
-                        modifier = Modifier.fillMaxWidth()
+                        modifier = Modifier
+                            .fillMaxWidth()
                             .height(emailHeightAnimation),
                         dismissState = dismissState
                     )
                 },
                 dismissContent = {
                     EmailItem(
-                        modifier = Modifier.fillMaxWidth()
-                        .height(emailHeightAnimation),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(emailHeightAnimation),
                         dismissState = dismissState,
                         email = email
                     )
                 }
+            )
+            Divider(modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .alpha(dividerAlpha)
             )
         }
     }
